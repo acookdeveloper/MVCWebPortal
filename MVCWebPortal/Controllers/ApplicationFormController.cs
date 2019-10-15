fffad517b369567e48b8a8ed91af6b33a9faade5
Ant C <anthonyrobertcook@gmail.com>
2019-10-15 22:49:45 +0100

    loops and html input tests

---
+++ MVCWebPortal/Controllers/ApplicationFormController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebPortal.Controllers
+{
+    public class ApplicationFormController : Controller
+    {
+        // GET: ApplicationForm
+        public ActionResult Index(string name, int numTimes = 1)
+        {
+            if (name != "")
+            {
+                ViewData["Message"] = "Hello " + name;
+            }
+            if (numTimes >= 1){
+                ViewData["NumTimes"] = numTimes;
+            }
+            if (name == "")
+            {
+                ViewData["Message"] = "No name has been entered.";
+            }
+            if (numTimes == 1)
+            {
+                ViewData["NumTimes"] = 0;
+            }
+
+            return View();
+        }
+    }
+}
